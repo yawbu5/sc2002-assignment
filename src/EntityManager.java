6c92408ec5044ebf2ae0e62a1c02295dae9b57c9
Kervin Troy <troy.malicdem@gmail.com>
2026-03-31 00:52:48 +0800

    first commit

--- src/EntityManager.java
+++ src/EntityManager.java
@@ -1,17 +1,21 @@
 package src;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntityManager {
-    final entities = <Entity>[];
-    final toAdd = <Entity>[];
-    final entityMap = Map<String, int>
-    private int counter = 0;
+    final ArrayList<Entity> entities = new ArrayList<>();
+    final ArrayList<Entity> toAdd = new ArrayList<>();
+    final Map<String, Integer> entityMap = new HashMap<>();
+    private int count = 0;
 
     public EntityManager() {
 
     }
 
-    private void _incr_count() {
-       return ++count; 
+    private int _incr_count() {
+       return count++;
     }
 
     public void DeleteEntity(int id) {
