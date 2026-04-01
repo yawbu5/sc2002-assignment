16e6073a051fc61c2135c219a8e015d9a86cf177
Kervin Troy <troy.malicdem@gmail.com>
2026-04-01 14:18:15 +0800

    JSONLoader unit test, added dummy test class

---
+++ tests/DummyClass.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class DummyClass {
+    public String name;
+    public int id;
+
+    public DummyClass(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ tests/data/JSONLoaderTest.java
@@ -0,0 +1,22 @@
+package tests.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+import src.data.JSONLoader;
+import tests.DummyClass;
+
+import java.util.List;
+
+public class JSONLoaderTest {
+
+    @Test
+    public void testLoadListFromJSON() {
+        List<DummyClass> dummies = JSONLoader.loadList("resources/dummy_data.json", DummyClass.class);
+
+        Assert.assertNotNull(dummies);
+        Assert.assertEquals("John", dummies.get(0).name);
+        Assert.assertEquals(2, dummies.get(1).id);
+        Assert.assertEquals("Charlie", dummies.get(2).name);
+        Assert.assertEquals(4, dummies.get(3).id);
+    }
+}
