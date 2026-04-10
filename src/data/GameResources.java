66573c9af4cd55ab586c419228cf8a000452d7ec
Kervin Troy <troy.malicdem@gmail.com>
2026-04-11 01:18:19 +0800

    resource loader

--- src/data/GameResources.java
+++ src/data/GameResources.java
@@ -2,19 +2,21 @@ package src.data;
 
 import src.Ability;
 import src.Entity;
+import src.Wave;
 
 import java.util.List;
 
 /**
  * Holds the loaded data
  */
-public class GameDB {
-    public final List<Ability> abilities;
-    public final List<Entity> entities;
-    // TODO: public final List<Wave> waves;
+public class GameResources {
+    public List<Ability> abilities;
+    public List<Entity> entities;
+    public List<Wave> waves;
 
-    public GameDB() {
+    public GameResources() {
         abilities = JSONLoader.loadList("resources/abilities.json", Ability.class);
         entities = JSONLoader.loadList("resources/entities.json", Entity.class);
+        waves = JSONLoader.loadList("resources/waves.json", Wave.class);
     }
 }
