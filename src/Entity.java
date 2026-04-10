b6bf57b9c5eb83f823c49930d1423faffa1e51d6
Kervin Troy <troy.malicdem@gmail.com>
2026-04-11 02:15:30 +0800

    ability list

--- src/Entity.java
+++ src/Entity.java
@@ -1,7 +1,9 @@
 package src;
 
+import java.util.List;
+
 public class Entity {
-    public enum EntityType {
+    private enum EntityType {
         ENEMY,
         PLAYER
     }
@@ -13,14 +15,20 @@ public class Entity {
     public int attack;
     public int defence;
     public int speed;
+    public List<String> abilities;
 
-    public Entity(String name, EntityType type, int hp, int attack, int defence, int speed) {
+    public Entity(String name, EntityType type, int hp, int attack, int defence, int speed, List<String> abilities) {
         this.name = name;
         this.type = type;
         this.maxHp = this.currHp = hp;
         this.attack = attack;
         this.defence = defence;
         this.speed = speed;
+        this.abilities = abilities;
+    }
+
+    public static Entity CreateEntity(Entity e) {
+        return new Entity(e.name, e.type, e.maxHp, e.attack, e.defence, e.speed, e.abilities);
     }
 
     void Update(Action a) {
