1169872aba5aee01ac9609c7431b950d96516c92
Kervin Troy <troy.malicdem@gmail.com>
2026-04-13 01:13:25 +0800

    Utilities for adding/removing entities

--- src/main/java/systems/EntityManager.java
+++ src/main/java/systems/EntityManager.java
@@ -2,6 +2,7 @@ package systems;
 
 import data.EntityTemplate;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +19,10 @@ public class EntityManager {
         return entityMap.size();
     }
 
-    public String AddEntity(EntityTemplate e ) {
+    public void AddEntity(EntityTemplate e) {
         Entity newEntity = Entity.CreateEntity(idCount, e);
         entityMap.put(idCount, newEntity);
         idCount++;
-
-        return (idCount + ": " + newEntity.getName());
     }
 
     public void AddEntitiesFromList(List<EntityTemplate> list) {
@@ -36,8 +35,38 @@ public class EntityManager {
         return entityMap.get(id);
     }
 
+    /**
+     * Use to get a List of entity(s) of a certain type.
+     * Returns a list of entites of that type.
+     * i.e., Retrieve a list of enemies.
+     * @param type EntityType
+     * @return List
+     */
+    public List<Entity> GetEntityByType(Entity.EntityType type) {
+        ArrayList<Entity> result = new ArrayList<>();
+        for (Entity e : entityMap.values()) {
+            if (e.getType() == type) {
+                result.add(e);
+            }
+        }
+
+        return result;
+    }
+
     public Entity RemoveEntity(int id) {
         return entityMap.remove(id);
+    }
+    /**
+     * Use for mass removal of entity(s) of the same type.
+     * Returns the number of entities left.
+     * i.e., Remove all enemies.
+     * @param type EntityType
+     * @return int
+    */
+    public int RemoveEntityByType(Entity.EntityType type) {
+        entityMap.entrySet().removeIf(entry -> type.equals(entry.getValue().getType()));
+
+        return this.count();
     }
 
 }
