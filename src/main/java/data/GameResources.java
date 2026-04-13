463b17f65a3ca461725b723d4f755600112f0f98
Kervin Troy <troy.malicdem@gmail.com>
2026-04-14 01:17:19 +0800

    Utility functions

--- src/main/java/data/GameResources.java
+++ src/main/java/data/GameResources.java
@@ -1,18 +1,26 @@
 package data;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Holds the loaded data
  */
 public class GameResources {
-    public final List<Ability> abilities;
+    public final List<ActionTemplate> abilities;
     public final List<EntityTemplate> entityTemplates;
     public final List<Wave> waves;
 
     public GameResources() {
-        abilities = JSONLoader.loadList("abilities.json", Ability.class);
+        abilities = JSONLoader.loadList("abilities.json", ActionTemplate.class);
         entityTemplates = JSONLoader.loadList("entities.json", EntityTemplate.class);
         waves = JSONLoader.loadList("waves.json", Wave.class);
+    }
+
+    public static <T> List<T> getListByPredicate(List<T> inputList, Predicate<T> pred) {
+        return inputList.stream()
+                .filter(pred)
+                .collect(Collectors.toList());
     }
 }
