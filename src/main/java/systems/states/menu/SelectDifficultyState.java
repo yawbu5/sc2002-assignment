96b82b5281758bbd84794b04d2c0490c33aee4b1
Kervin Troy <troy.malicdem@gmail.com>
2026-04-14 01:19:04 +0800

    Additional wave descriptions

--- src/main/java/systems/states/menu/SelectDifficultyState.java
+++ src/main/java/systems/states/menu/SelectDifficultyState.java
@@ -2,16 +2,15 @@ package systems.states.menu;
 
 import commands.Command;
 import commands.StartMenuCommand;
-import data.Ability;
 import data.EntityTemplate;
 import data.Wave;
 import systems.BattleEngine;
-import systems.Entity;
-import systems.states.BattleState;
 import systems.states.GameState;
+import systems.states.battle.InitialiseState;
 import ui.GameView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SelectDifficultyState implements GameState {
@@ -25,14 +24,41 @@ public class SelectDifficultyState implements GameState {
         List<Wave> waves = engine.retrieveDbWaves();
         List<Command> difficultyChoices = new ArrayList<>();
 
+        List<String> enemyAttributes = new ArrayList<>();
         for (Wave w : waves) {
-            difficultyChoices.add(new StartMenuCommand(w.name, () -> engine.setDifficulty(w)));
+            int enemyCount = 0;
+
+            List<String> uniqueEntities = new ArrayList<>();
+            for (List<String> l : w.waves) {
+                for (String s : l) {
+                    if (!uniqueEntities.contains(s)) {
+                        uniqueEntities.add(s);
+                    }
+                    enemyCount++;
+                }
+            }
+
+            StringBuilder enemies = new StringBuilder();
+
+            for (String s : uniqueEntities) {
+                EntityTemplate ent = engine.retrieveDbEntity(s);
+                enemies.append(ent.name).append(", ");
+                enemyAttributes.add(ent.name + " | HP: " + ent.hp + ", DEF: " + ent.defence + ", SPD: " + ent.speed);
+            }
+
+            difficultyChoices.add(new StartMenuCommand((w.name + " | Number of Enemies: " + enemyCount + "| Enemies: " + enemies), () -> engine.setDifficulty(w)));
         }
 
         Command selected = view.PromptUserChoice("Select a difficulty: ", difficultyChoices);
+
+        //view.DisplayMessage("Enemy attributes: ");
+        //for (String s : enemyAttributes) {
+        //    view.DisplayMessage(s);
+        //}
+
         selected.execute(null);
 
-        return new SelectCharacterState();
+        return new InitialiseState();
     }
 
     @Override
