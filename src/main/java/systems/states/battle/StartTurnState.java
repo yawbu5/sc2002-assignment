5f227bf62f7a5ee3662e8cdbda9a2dc5fa4c9a7a
Kervin Troy <troy.malicdem@gmail.com>
2026-04-14 01:18:28 +0800

    StartTurn logic

--- src/main/java/systems/states/battle/StartTurnState.java
+++ src/main/java/systems/states/battle/StartTurnState.java
@@ -1,26 +1,61 @@
 package systems.states.battle;
 
 import systems.BattleEngine;
+import systems.Entity;
 import systems.states.GameState;
 import ui.GameView;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * Responsibility: Determines turn order, manages game ticks.
+ * Responsibility: Determines turn order
  */
 public class StartTurnState implements GameState {
     @Override
-    public void onEnter(BattleEngine engine, GameView view) {
-
-    }
-
-    @Override
     public GameState onUpdate(BattleEngine engine, GameView view) {
-        // Load abilities as known actions
-        return null;
-    }
+        /*
+         Only go through entities that are alive.
+         We try not to delete entities until a wave/battle is over.
+         This allows for implementation of a potential revive function
+         and prevents any potential weird side effects.
+        */
+        List<Entity> alive = engine.getEntityManager().getAliveEntities();
 
-    @Override
-    public void onExit(BattleEngine engine, GameView view) {
+        /*
+        We sort the playable (alive) entities by their current speed.
+         */
+        List<Integer> currentTurnOrder = engine.getTurnOrder();
+        if (currentTurnOrder == null) {
+            // Handle very first turn, order turns by SPD
+            alive.sort(Comparator.comparing(Entity::getSpeed).reversed());
 
+            // Transforms list of Entity data into a simple list of Entity Ids. Smaller signature, more performant.
+            List<Integer> newTurnOrder = alive.stream().map(Entity::getId).collect(Collectors.toList());
+
+            engine.setTurnOrder(newTurnOrder);
+        } else {
+            // What do we expect after EndTurn? Win conditions checked.
+            // Possible scenarios after that point: Either an enemy killed or not killed.
+
+            // Either way we just queue and dequeue the first element (i.e., the last played)
+           currentTurnOrder.add(currentTurnOrder.get(0));
+           currentTurnOrder.remove(0);
+
+           // set to new turn order: next in line.
+           engine.setTurnOrder(currentTurnOrder);
+        }
+
+        // if player is first, go to player
+        // if not, go to enemy
+        // A bit lengthy, but it does the job
+        if (engine.getEntityManager().getEntity(engine.getFirstTurnEntity()).getType() == Entity.EntityType.PLAYER) {
+            view.DisplayMessage(engine.getTurnOrder().toString());
+            return new PlayerTurnState();
+        } else {
+            view.DisplayMessage(engine.getTurnOrder().toString());
+            return new EnemyTurnState();
+        }
     }
 }
