fcb2d6c6ae9891f578b3a2c081ff2afe1d6672bb
Kervin Troy <troy.malicdem@gmail.com>
2026-04-14 01:14:23 +0800

    Changed wave schema

--- src/main/java/data/Wave.java
+++ src/main/java/data/Wave.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class Wave {
     public final String name;
-    public final List<List<Integer>> waves;
+    public final List<List<String>> waves;
 
     /**
      * Data template class for Wave data.
      * May be used as is rather than a data template due to simplicity.
      */
-    public Wave(String name, List<List<Integer>> waves) {
+    public Wave(String name, List<List<String>> waves) {
         this.name = name;
         this.waves = waves;
     }
