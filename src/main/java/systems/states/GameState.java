e766c916fc4e6c0884a9b8a6aba411e1baff1111
Kervin Troy <troy.malicdem@gmail.com>
2026-04-14 01:16:54 +0800

    Swapped defaults to more sensible ones

--- src/main/java/systems/states/GameState.java
+++ src/main/java/systems/states/GameState.java
@@ -3,10 +3,22 @@ package systems.states;
 import systems.BattleEngine;
 import ui.GameView;
 
+/**
+ * GameState defines a certain unique phase,behaviour or action that is entered into
+ * by either another state or the base game loop.
+ * <p>
+ * onUpdate MUST be defined, while onEnter and onExit are optional.
+ * This behaviour should be reflected in the implemented code, where we may call those optional
+ * states dynamically without necessarily expecting a result or change.
+ */
 public interface GameState {
-    void onEnter(BattleEngine engine, GameView view);
-    default GameState onUpdate(BattleEngine engine, GameView view) {
-        return null;
+    // default methods (onEnter, onExit) treated as optional
+    default void onEnter(BattleEngine engine, GameView view) {
     }
-    void onExit(BattleEngine engine, GameView view);
+
+    default void onExit(BattleEngine engine, GameView view) {
+    }
+
+    // MUST define an implementation for onUpdate
+    GameState onUpdate(BattleEngine engine, GameView view);
 }
