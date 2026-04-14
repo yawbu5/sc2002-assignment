3bda09e8537a9bcbbf4cd88e0559d02a25e8a6f7
Kervin Troy <troy.malicdem@gmail.com>
2026-04-14 23:15:26 +0800

    Implemented Observer code

--- src/main/java/ui/console/ConsoleView.java
+++ src/main/java/ui/console/ConsoleView.java
@@ -1,13 +1,15 @@
 package ui.console;
 
 import commands.Command;
+import observable.BattleObserver;
+import observable.MenuObserver;
 import systems.BattleEngine;
 import ui.GameView;
 
 import java.util.List;
 import java.util.Scanner;
 
-public class ConsoleView implements GameView {
+public class ConsoleView implements GameView, MenuObserver, BattleObserver {
     private BattleEngine engine;
     private final Scanner sc;
     private boolean running;
@@ -22,6 +24,8 @@ public class ConsoleView implements GameView {
         }
 
         this.engine = engine;
+        this.engine.addMenuObserver(this);
+        this.engine.addBattleObserver(this);
     }
 
     public void DisplayMessage(String txt) {
@@ -34,16 +38,6 @@ public class ConsoleView implements GameView {
         }
     }
 
-    @Override
-    public void Update() {
-
-    }
-
-    @Override
-    public Command PromptUserInput(String msg, Command cmd) {
-        return null;
-    }
-
     public Command PromptUserChoice(String msg, List<Command> options) {
         int input = -1;
         boolean entered = false;
@@ -51,7 +45,7 @@ public class ConsoleView implements GameView {
             if (!entered) {
                 entered = true;
             } else {
-                System.out.println("Erroneus input, try again!");
+                System.out.println("Erroneous input, try again!");
             }
             System.out.println(msg);
 
@@ -66,5 +60,50 @@ public class ConsoleView implements GameView {
         }
 
         return options.get(input - 1);
+    }
+
+    @Override
+    public void OnLogAction(String msg) {
+
+    }
+
+    @Override
+    public void OnWaveSpawn(int waveNo) {
+
+    }
+
+    @Override
+    public void OnEntitySpawn(String entId) {
+
+    }
+
+    @Override
+    public void onChoicePrompt(String msg, List<Command> options) {
+        int input = -1;
+        boolean entered = false;
+        while (input < 1 || input > options.size()) {
+            if (!entered) {
+                entered = true;
+            } else {
+                System.out.println("Erroneous input, try again!");
+            }
+            System.out.println(msg);
+
+            for (int i = 0; i < options.size(); i++) {
+                System.out.println((i + 1) + ": " + options.get(i).getDisplayText());
+            }
+            System.out.print("> ");
+            try {
+                input = Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException ignored) {
+            }
+
+            engine.queueNextCommand(options.get(input - 1));
+        }
+    }
+
+    @Override
+    public void onDisplayMessage(String msg) {
+        System.out.println(msg);
     }
 }
