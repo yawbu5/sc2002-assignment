e49014aead442d2107e73bdb338547bb33d3e5fb
Kervin Troy <troy.malicdem@gmail.com>
2026-04-17 05:00:21 +0800

    Inventory open command

---
+++ src/main/java/commands/OpenInventoryCommand.java
@@ -0,0 +1,17 @@
+package commands;
+
+import systems.BattleEngine;
+
+/**
+ * Simple message to open the inventory, purely an identifier and no other function.
+ */
+public class OpenInventoryCommand implements Command{
+    @Override
+    public String getDisplayText() {
+        return "Inventory";
+    }
+
+    @Override
+    public void execute(BattleEngine engine) {
+    }
+}
