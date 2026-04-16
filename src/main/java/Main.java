f128f615f81971aa25da3a26c03bb6c4c3e74f9a
Kervin Troy <troy.malicdem@gmail.com>
2026-04-17 04:59:54 +0800

    More appropiate view names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 import data.GameResources;
 import systems.BattleEngine;
-import ui.GameView;
-import ui.console.ConsoleView;
+import view.console.ConsoleView;
+
 
 public class Main {
     public static void main(String[] args) {
         GameResources db = new GameResources();
         BattleEngine engine = new BattleEngine(db);
-        GameView view = new ConsoleView();
+        ConsoleView view = new ConsoleView();
 
         view.connectEngine(engine);
 
